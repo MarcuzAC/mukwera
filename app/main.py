2cdcd196e8b023bd9c149c0d09f7dbc8464bff9f
Milton Chithenga <88409286+MarcuzAC@users.noreply.github.com>
2025-07-11 22:50:28 +0200

    added build command

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth,rides, users
@@ -21,3 +22,8 @@ app.include_router(rides.router, prefix="/rides", tags=["Rides"])
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Mukwera API"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
